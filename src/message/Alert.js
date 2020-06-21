a5a13a0c968f0c1852c2aa7e42e53ac4269433fd
Aleksi Lindblad <aleksi.lindblad@gmail.com>
2020-06-21 19:56:28 +0300

    Moved reminders to alerts

--- src/message/Alert.js
+++ src/message/Alert.js
@@ -1,18 +1,28 @@
 import React, {useEffect, useState} from 'react';
 import * as process from "../process";
+import alertSound from "../pling.wav";
 
 function Alert() {
     const [alert, setAlert] = useState(null);
+    let hideAlertTimer = null;
+    const ALERT_HIDE_TIME = 5 * 1000;
 
     useEffect(() => {
-        process.onAlert(alert => {
-            setAlert(alert);
-        })
+        process.onAlert(newAlert);
+
+        return () => {
+            clearTimeout(hideAlertTimer);
+        }
     });
 
-    return (
-        <div>{alert}</div>
-    );
+    const newAlert  = alert => {
+        let audio = new Audio(alertSound);
+        audio.play();
+        setAlert(alert);
+        hideAlertTimer = setTimeout(() => setAlert(null), ALERT_HIDE_TIME);
+    }
+
+    return <div>{alert}</div>;
 }
 
 export default Alert;
--- src/task/TaskHandler.js
+++ src/task/TaskHandler.js
@@ -1,49 +1,24 @@
 import React, {useEffect, useRef, useState} from "react";
-import plingSound from "../pling.wav";
 import * as process from "../process";
 import {
     ChooseInstrumentType, SimpleTask, YesNoTask
 } from "./Tasks";
 
-function playAlarm() {
-    let audio = new Audio(plingSound);
-    audio.play();
-}
 
 function TaskHandler({song, updateSongHandler}) {
     const [task, setTask] = useState(null);
-    const [reminder, setReminder] = useState(false);
     const timerRef = useRef(null);
 
     useEffect(() => {
-        const REMINDER_TIME = 5 * 60 * 1000;
-        // const REMINDER_TIME = 10 * 1000;
-        const REMINDER_HIDE_TIME = 5 * 1000;
-        let hideReminderTimer = null;
-        let showReminderTimer = null;
-        setReminder(false);
-
-        const remind = () => {
-            playAlarm();
-            setReminder(true);
-            hideReminderTimer = setTimeout(() => setReminder(false), REMINDER_HIDE_TIME);
-            showReminderTimer = setTimeout(remind, REMINDER_TIME);
-        }
-
         if (task == null) {
             waitForTasks(song.id, newTask => {
                 setTask(newTask);
             }, timerRef);
-        } else {
-            playAlarm();
-            showReminderTimer = setTimeout(remind, REMINDER_TIME);
         }
 
         return () => {
             // eslint-disable-next-line react-hooks/exhaustive-deps
             clearTimeout(timerRef.current);
-            clearTimeout(showReminderTimer);
-            clearTimeout(hideReminderTimer);
         }
     }, [song.id, task]);
 
@@ -69,7 +44,7 @@ function TaskHandler({song, updateSongHandler}) {
     }
 
     const TaskComponent = (task != null) ? getTaskComponent(task.formKey) : null;
-    return <div>{reminder ? <div>Back to work!</div> : null}{(TaskComponent != null) ? <TaskComponent doTask={doTask} song={song} task={task}/> : "Waiting for next task"}</div>
+    return <div>{(TaskComponent != null) ? <TaskComponent doTask={doTask} song={song} task={task}/> : "Waiting for next task"}</div>
 }
 
 function waitForTasks(songId, callback, timerRef) {
